7d14b46c753b9f0b447659764640e460fdadae16
FragLegs <miel.shayne@gmail.com>
2019-05-09 21:48:55 -0400

    Made loader and train work with MRNet data

--- scripts/loader.py
+++ scripts/loader.py
@@ -1,44 +1,69 @@
+import logging
+
 import numpy as np
-import os
-import pickle
+import pandas as pd
 import torch
 import torch.nn.functional as F
 import torch.utils.data as data
 
 from torch.autograd import Variable
 
+
+log = logging.getLogger(__name__)
+
 INPUT_DIM = 224
 MAX_PIXEL_VAL = 255
 MEAN = 58.09
 STDDEV = 49.73
 
-BASE_DATA_DIR = '/domino/datasets/local/raw'
+
+def load_volume(path):
+    vol = np.load(path)
+
+    # crop middle
+    pad = int((vol.shape[2] - INPUT_DIM) / 2)
+    vol = vol[:, pad:-pad, pad:-pad]
+
+    # standardize
+    vol = (vol - np.min(vol)) / (np.max(vol) - np.min(vol)) * MAX_PIXEL_VAL
+
+    # normalize
+    vol = (vol - MEAN) / STDDEV
+
+    # convert to RGB
+    vol = np.stack((vol,) * 3, axis=1)
+
+    return torch.FloatTensor(vol)
 
 
 class Dataset(data.Dataset):
-    def __init__(self, datadirs, diagnosis, use_gpu):
+    def __init__(self,
+                 series,
+                 path_df,
+                 diagnosis=None,
+                 label_df=None,
+                 use_gpu=True):
         super().__init__()
         self.use_gpu = use_gpu
+        self.series = series
 
-        label_dict = {}
-        self.paths = []
+        self.paths = {
+            idx: row[self.series] for idx, row in path_df.iterrows()
+        }
+        self.labels = None
 
-        for i, line in enumerate(open('metadata.csv').readlines()):
-            if i == 0:
-                continue
-            line = line.strip().split(',')
-            path = line[10]
-            label = line[2]
-            label_dict[path] = int(int(label) > diagnosis)
+        if label_df is not None:
+            # we only need to keep paths for the relevant labels
+            self.paths = {idx: self.paths[idx] for idx in label_df.index}
 
-        for dir in datadirs:
-            for file in os.listdir(os.path.join(BASE_DATA_DIR, dir)):
-                self.paths.append(dir + '/' + file)
+        if diagnosis is not None:
+            self.diagnosis = diagnosis
+            self.labels = {
+                idx: row[self.diagnosis] for idx, row in label_df.iterrows()
+            }
 
-        self.labels = [label_dict[path[6:]] for path in self.paths]
-
-        neg_weight = np.mean(self.labels)
-        self.weights = [neg_weight, 1 - neg_weight]
+            neg_weight = np.mean(self.labels.values())
+            self.weights = [neg_weight, 1 - neg_weight]
 
     def weighted_loss(self, prediction, target):
         weights_npy = np.array([self.weights[int(t[0])] for t in target.data])
@@ -51,28 +76,12 @@ class Dataset(data.Dataset):
         return loss
 
     def __getitem__(self, index):
-        path = self.paths[index]
-        real_path = os.path.join(BASE_DATA_DIR, path)
+        vol_tensor = load_volume(self.paths[index])
 
-        # Must use rb - data is binary
-        with open(real_path, 'rb') as file_handler:
-            vol = pickle.load(file_handler).astype(np.int32)
-
-        # crop middle
-        pad = int((vol.shape[2] - INPUT_DIM) / 2)
-        vol = vol[:, pad:-pad, pad:-pad]
-
-        # standardize
-        vol = (vol - np.min(vol)) / (np.max(vol) - np.min(vol)) * MAX_PIXEL_VAL
-
-        # normalize
-        vol = (vol - MEAN) / STDDEV
-
-        # convert to RGB
-        vol = np.stack((vol,) * 3, axis=1)
-
-        vol_tensor = torch.FloatTensor(vol)
-        label_tensor = torch.FloatTensor([self.labels[index]])
+        label_tensor = (
+            None if self.labels is None else
+            torch.FloatTensor([self.labels[index]])
+        )
 
         return vol_tensor, label_tensor
 
@@ -80,14 +89,65 @@ class Dataset(data.Dataset):
         return len(self.paths)
 
 
-def load_data(diagnosis, use_gpu=False):
-    train_dirs = ['vol08', 'vol04', 'vol03', 'vol09', 'vol06', 'vol07']
-    valid_dirs = ['vol10', 'vol05']
-    test_dirs = ['vol01', 'vol02']
+def paths_to_df(paths):
+    df = pd.DataFrame()
 
-    train_dataset = Dataset(train_dirs, diagnosis, use_gpu)
-    valid_dataset = Dataset(valid_dirs, diagnosis, use_gpu)
-    test_dataset = Dataset(test_dirs, diagnosis, use_gpu)
+    for path in paths:
+        series, case = path.split('/')[-2:]
+        df.loc[case, series] = path
+
+    assert(np.all(pd.notnull(df).values))
+    return df
+
+
+def load_data(paths,
+              series,
+              label_df=None,
+              diagnosis=None,
+              use_gpu=False,
+              is_full=False):
+
+    path_df = paths_to_df(paths)
+
+    # For inference
+    if label_df is None:
+        dataset = Dataset(
+            series=series,
+            path_df=path_df,
+            diagnosis=diagnosis,
+            label_df=label_df,
+            use_gpu=use_gpu
+        )
+        loader = data.DataLoader(
+            dataset, batch_size=1, num_workers=1, shuffle=False
+        )
+        return loader, None, None
+
+    if is_full:
+        train_df = label_df[label_df.split in ['train', 'valid']]
+        valid_df = label_df[label_df.split == 'test']
+        test_df = None
+    else:
+        train_df = label_df[label_df.split == 'train']
+        valid_df = label_df[label_df.split == 'valid']
+        test_df = label_df[label_df.split == 'test']
+
+    train_dataset = Dataset(
+        series=series,
+        path_df=path_df,
+        diagnosis=diagnosis,
+        label_df=train_df,
+        use_gpu=use_gpu
+    )
+    log.debug('Train dataset had {} instances'.format(len(train_dataset)))
+    valid_dataset = Dataset(
+        series=series,
+        path_df=path_df,
+        diagnosis=diagnosis,
+        label_df=valid_df,
+        use_gpu=use_gpu
+    )
+    log.debug('Valid dataset had {} instances'.format(len(valid_dataset)))
 
     train_loader = data.DataLoader(
         train_dataset, batch_size=1, num_workers=1, shuffle=True
@@ -95,8 +155,20 @@ def load_data(diagnosis, use_gpu=False):
     valid_loader = data.DataLoader(
         valid_dataset, batch_size=1, num_workers=1, shuffle=False
     )
-    test_loader = data.DataLoader(
-        test_dataset, batch_size=1, num_workers=1, shuffle=False
-    )
+
+    if test_df is not None:
+        test_dataset = Dataset(
+            series=series,
+            path_df=path_df,
+            diagnosis=diagnosis,
+            label_df=test_df,
+            use_gpu=use_gpu
+        )
+        log.debug('Test dataset had {} instances'.format(len(test_dataset)))
+        test_loader = data.DataLoader(
+            test_dataset, batch_size=1, num_workers=1, shuffle=False
+        )
+    else:
+        test_loader = None
 
     return train_loader, valid_loader, test_loader
--- scripts/train.py
+++ scripts/train.py
@@ -1,11 +1,14 @@
 import argparse
+from datetime import datetime
 import json
-import numpy as np
+import logging
 import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 import torch
 
-from datetime import datetime
-from pathlib import Path
 # from sklearn import metrics
 
 from evaluate import run_model
@@ -13,19 +16,45 @@ from loader import load_data
 from model import MRNet
 
 
-def train(rundir, diagnosis, epochs, learning_rate, use_gpu):
-    train_loader, valid_loader, test_loader = load_data(diagnosis, use_gpu)
+MODELS = {
+    'MRNet': MRNet
+}
 
-    model = MRNet()
 
-    if use_gpu:
+def train(model_name,
+          rundir,
+          diagnosis,
+          series,
+          epochs,
+          learning_rate,
+          weight_decay,
+          max_patience,
+          factor,
+          full,
+          gpu,
+          **kwargs):
+
+    # load the paths dataframe
+    paths = pd.read_csv('/mnt/mrnet-image-paths.csv').values
+
+    # load the labels dataframe
+    label_df = pd.read_csv('/mnt/mrnet-labels.csv', index_col=0)
+
+    train_loader, valid_loader, _ = load_data(
+        paths=paths, series=series, label_df=label_df,
+        diagnosis=diagnosis, use_gpu=gpu, is_full=full
+    )
+
+    model = MODELS[model_name]()  # MRNet()
+
+    if gpu:
         model = model.cuda()
 
     optimizer = torch.optim.Adam(
-        model.parameters(), learning_rate, weight_decay=.01
+        model.parameters(), learning_rate, weight_decay=weight_decay
     )
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, patience=5, factor=.3, threshold=1e-4
+        optimizer, patience=max_patience, factor=factor, threshold=1e-4
     )
 
     best_val_loss = float('inf')
@@ -60,33 +89,71 @@ def train(rundir, diagnosis, epochs, learning_rate, use_gpu):
             torch.save(model.state_dict(), save_path)
 
 
-def get_parser():
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rundir', type=str, required=True)
-    parser.add_argument('--diagnosis', type=int, required=True)
+    parser.add_argument('model_name')
+    parser.add_argument(
+        '--rundir', type=str, default='runs/{model_name}/{series}/{diagnosis}'
+    )
+    parser.add_argument(
+        '-d',
+        '--diagnosis',
+        choices=['abnormal', 'acl', 'meniscus', 'all'],
+        default='all'
+    )
+    parser.add_argument(
+        '-s',
+        '--series',
+        choices=['axial', 'coronal', 'sagittal', 'all'],
+        default='all'
+    )
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('--full', action='store_true')
     parser.add_argument('--learning_rate', default=1e-05, type=float)
     parser.add_argument('--weight_decay', default=0.01, type=float)
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--max_patience', default=5, type=int)
     parser.add_argument('--factor', default=0.3, type=float)
-    return parser
+
+    verbosity_help = 'Verbosity level (default: %(default)s)'
+    choices = [
+        logging.getLevelName(logging.DEBUG),
+        logging.getLevelName(logging.INFO),
+        logging.getLevelName(logging.WARN),
+        logging.getLevelName(logging.ERROR)
+    ]
+
+    parser.add_argument(
+        '-v',
+        '--verbosity',
+        choices=choices,
+        help=verbosity_help,
+        default=logging.getLevelName(logging.INFO)
+    )
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Set the logging to console level
+    logging.basicConfig(level=args.verbosity)
+
+    return args
 
 
 if __name__ == '__main__':
-    args = get_parser().parse_args()
+    args = parse_args()
 
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     if args.gpu:
         torch.cuda.manual_seed_all(args.seed)
 
+    args.rundir = args.rundir.format(**args.__dict__)
+
     os.makedirs(args.rundir, exist_ok=True)
 
     with open(Path(args.rundir) / 'args.json', 'w') as out:
         json.dump(vars(args), out, indent=4)
 
-    train(
-        args.rundir, args.diagnosis, args.epochs, args.learning_rate, args.gpu
-    )
+    train(**args.__dict__)
