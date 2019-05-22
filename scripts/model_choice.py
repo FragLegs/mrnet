ea7ee82fa109cde136e76fa1d6733a19065d78a6
FragLegs <miel.shayne@gmail.com>
2019-05-22 13:49:58 -0400

    add MRNet-Res-7-drop

--- scripts/model_choice.py
+++ scripts/model_choice.py
@@ -15,6 +15,7 @@ MODELS = {
     'MRNet-Res-7-1': MRNetRes7_1,
     'MRNet-Res-7-1-until6': MRNetRes7_1,
     'MRNet-Res-7-1-conv2': MRNetRes7_1_conv2,
+    'MRNet-Res-7-drop': MRNetRes7Dropout,
 }
 
 
